4e0fe1936bf6c63b174b9f72ff9629426e7c9fba
Cheris Quessou <quessoucheris144@gmail.com>
2024-09-06 11:26:24 +0100

    added more code

--- script.js
+++ script.js
@@ -34,10 +34,28 @@ function validatePhone(){
     }
 
     if(!phone.match(/^[0-9]{10}$/)){
-        phoneError.innerHTML = 'Phone no is required';
+        phoneError.innerHTML = 'Only digits please';
         return false;
     }
 
     phoneError.innerHTML = '<i class="fa fa-check-circle"></i>';
+    return true;
+}
+
+
+function validateEmail(){
+    var email = document.getElementById('contact-email').value;
+
+    if(email.length == 0){
+        emailError.innerHTML = 'Email is required'
+        return false;
+    }
+
+    if(!email.match(/^[A-Za-z]\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)){
+        emailError.innerHTML = 'Email Invalid'
+        return false;
+    }
+    
+    emailError.innerHTML = '<i class="fa fa-check-circle"></i>';
     return true;
 }
